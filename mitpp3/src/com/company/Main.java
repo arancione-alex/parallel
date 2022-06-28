7941d12145fd1e1f7af30704b06be59dcefe2264
arancione-alex <58134695+arancione-alex@users.noreply.github.com>
2022-06-28 16:35:52 +0300

    Add files via upload

--- mitpp3/src/com/company/Main.java
+++ mitpp3/src/com/company/Main.java
@@ -7,10 +7,12 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        int iterations = 200;
-        long timeMs = 2000;
-        int cells = 20;
-        int parts = 6;
+        int iterations = 10000;
+        long timeMs = 200;
+        long delay = 10;
+        int cells = 10;
+        int parts = 8;
+
         List<int[]> crystal = new ArrayList<>();
         for(int i = 0; i<cells;i++){
             int[] cell = new int[parts];
@@ -23,15 +25,38 @@ public class Main {
                 crystal.add(cell);
             }
         }
-        for(int z = 0; z< crystal.size();z++){
-            System.out.println(Arrays.toString(crystal.get(z)));
-        }
-        System.out.println("-------------");
-        //MultipleThreadIterationMove.iterationMove(crystal,iterations);
-        MulltipleThreadTimeMove.timeMove(crystal,timeMs);
-        for(int z = 0; z< crystal.size();z++){
-            System.out.println(Arrays.toString(crystal.get(z)));
-        }
+        //MultipleThreadIterationMove.iterationMove(crystal,iterations);// cold start
+        MulltipleThreadTimeMove.timeMove(crystal,timeMs,delay);
 
+
+            List<int[]> crystal1 = new ArrayList<>();
+            for(int i = 0; i<cells;i++){
+                int[] cell = new int[10];
+                if(i==0){
+                    Arrays.fill(cell, 1);
+                    crystal1.add(cell);
+                }
+                else {
+                    Arrays.fill(cell, 0);
+                    crystal1.add(cell);
+                }
+            }
+//            long start = System.currentTimeMillis();
+//            MultipleThreadIterationMove.iterationMove(crystal,iterations);
+//            long end = System.currentTimeMillis();
+//            System.out.println("Estimated time for "+10+" threads is "+(end-start)+" ms");
+        System.out.println("---------------");
+        showRes(crystal);
+    }
+    public static void showRes(List<int[]> crystal){
+        List<Integer> res = new ArrayList<>();
+        for(int z = 0; z< crystal.size();z++){
+            int sum = 0;
+            for(int j = 0; j<crystal.get(z).length;j++){
+                sum += crystal.get(z)[j];
+            }
+            res.add(sum);
+        }
+        System.out.println(res);
     }
 }
